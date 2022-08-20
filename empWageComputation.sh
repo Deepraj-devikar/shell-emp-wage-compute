a061917c2a91f0ef66ddb0290780e91eb6bebd3a
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-20 18:25:22 +0530

    REFACTOR: resolve conflict

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,20 +11,25 @@ IS_PART_TIME=2
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=4
+totalEmpWage=0
 
-case $(attendance) in
-	$IS_PRESENT)
-		echo "Employee is present"
-		empWage=$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
-	;;
-	$IS_PART_TIME)
-		echo "Employee is part time"
-		empWage=$(($WAGE_PER_HOUR*$PART_TIME_HOUR))
-	;;
-	*)
-		echo "Employee is absent"
-		empWage=0
-	;;
-esac
+for (( day=1; day<=20; day++ ))
+do
+	case $(attendance) in
+		$IS_PRESENT)
+			echo "Employee is present"
+			empWage=$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
+		;;
+		$IS_PART_TIME)
+			echo "Employee is part time"
+			empWage=$(($WAGE_PER_HOUR*$PART_TIME_HOUR))
+		;;
+		*)
+			echo "Employee is absent"
+			empWage=0
+		;;
+	esac
+	totalEmpWage=$(($totalEmpWage+$empWage))
+done
 
-echo "Employee wage is $empWage"
+echo "Employee wage for month is $totalEmpWage"
