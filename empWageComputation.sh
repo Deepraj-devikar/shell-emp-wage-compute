72ff222b3db8e8fbd268b2a24dacceb296b060f1
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-20 18:34:56 +0530

    REFACTOR: add monthly hour limit

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,17 +11,22 @@ IS_PART_TIME=2
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=4
+MONTHLY_HOUR_LIMIT=100
 totalEmpWage=0
+totalHour=0
 
-for (( day=1; day<=20; day++ ))
+day=1
+while [ $day -le 20 -a $totalHour -le $MONTHLY_HOUR_LIMIT ]
 do
 	case $(attendance) in
 		$IS_PRESENT)
 			echo "Employee is present"
+			totalHour=$(($totalHour+$FULL_DAY_HOUR))
 			empWage=$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
 		;;
 		$IS_PART_TIME)
 			echo "Employee is part time"
+			totalHour=$(($totalHour+$PART_TIME_HOUR))
 			empWage=$(($WAGE_PER_HOUR*$PART_TIME_HOUR))
 		;;
 		*)
@@ -30,6 +35,7 @@ do
 		;;
 	esac
 	totalEmpWage=$(($totalEmpWage+$empWage))
+	day=$(($day+1))
 done
 
 echo "Employee wage for month is $totalEmpWage"
