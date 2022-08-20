8977b03d979925888194ba23087124086eefe775
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-20 11:36:56 +0530

    ADDED: Use case 1 employee attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,17 @@
 
 echo "Welcome to employee wage computation"
 
+function attendance () {
+	echo $((RANDOM%2))
+}
+
+IS_PRESENT=1
+
+if [ $(attendance) -eq $IS_PRESENT ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
+
+
