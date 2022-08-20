9a70b4c8ee9401fe4e735479ff989c35b1d80b5f
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-20 12:15:26 +0530

    ADDED: Calculation Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,12 +7,16 @@ function attendance () {
 }
 
 IS_PRESENT=1
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
 
 if [ $(attendance) -eq $IS_PRESENT ]
 then
 	echo "Employee is present"
+	empWage=$(($WAGE_PER_HOUR*$FULL_DAY_HOUR))
 else
 	echo "Employee is absent"
+	empWage=0
 fi
 
-
+echo "Employee wage is $empWage"
